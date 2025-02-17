7fc8f41412655ecbb28470678d83637bf77b15a6
ErickWarmling <warmling.erick@gmail.com>
2025-02-17 09:13:02 -0300

    Consulta TCC's
    Inserimento da funcionalidade Consulta de TCC's

--- assets/js/tcc.js
+++ assets/js/tcc.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function(){
     const id_tabela = "2PACX-1vSDF0f-Bc6daeqKfWjAaGjG14a0alpBCjTHYtvhxgOtpN2t9Tb8igm3vOqq9fqnEHJqS0OrnVhFrnCY";
     const tabela_url = `https://docs.google.com/spreadsheets/d/e/${id_tabela}/pub?output=csv`;
 
     fetch(tabela_url)
         .then(response => response.text())
-        .then(csvData =>{
+        .then(csvData => {
             const parsedData = Papa.parse(csvData, { header: false }).data;
             parsedData.shift();
 
-            $("#tabela-tcc").DataTable({
+            new DataTable("#tabela-tcc", {
                 data: parsedData,
                 columns: [
                     { title: "Aluno(a)" },
@@ -24,7 +24,7 @@ $(document).ready(function () {
                     url: "https://cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
                 },
                 order: [[1, 'asc']],
-                responsive: true
+                responsive: true,
             });
         })
         .catch(error => console.error("Erro ao carregar os dados da Planilha: ", error));
