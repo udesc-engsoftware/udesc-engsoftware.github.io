003a3b4c98df43d673d4fc2642597553f938e694
ErickWarmling <warmling.erick@gmail.com>
2025-02-17 16:44:59 -0300

    Update tcc.js

--- assets/js/tcc.js
+++ assets/js/tcc.js
@@ -2,14 +2,37 @@ document.addEventListener("DOMContentLoaded", function(){
     const id_tabela = "2PACX-1vSDF0f-Bc6daeqKfWjAaGjG14a0alpBCjTHYtvhxgOtpN2t9Tb8igm3vOqq9fqnEHJqS0OrnVhFrnCY";
     const tabela_url = `https://docs.google.com/spreadsheets/d/e/${id_tabela}/pub?output=csv`;
 
+    /* Busca os dados da Planilha  */
     fetch(tabela_url)
         .then(response => response.text())
         .then(csvData => {
             const parsedData = Papa.parse(csvData, { header: false }).data;
             parsedData.shift();
 
+            const links = parsedData.map(row => {
+                const titulo = row[1];
+                
+                const titulo_tcc = titulo.toLowerCase() /*Transforma em letras minúsculas*/
+                    .normalize("NFD").replace(/[\u0300-\u036f]/g, "") /* Separa e remove os acentos*/
+                    .replace(/[^a-z0-9]+/g, "-") /* Substitui caracteres inválidos por hífen "-" */ 
+                    .replace(/^-+|-+$/g, ""); /* Remove hífens no início e no final */
+                
+                const link_tcc = `/banca-tcc/tccs/${titulo_tcc}/`;
+                row[1] = `<a href="${link_tcc}" target="_blank">${titulo}</a>`;
+
+                return row;
+            });
+
+            console.log("Links dos TCC's");
+            links.forEach(row => {
+                const link = row[1].match(/href="([^"]+)"/);
+                if (link) {
+                    console.log(link[1]); 
+                }
+            });
+
             new DataTable("#tabela-tcc", {
-                data: parsedData,
+                data: links,
                 columns: [
                     { title: "Aluno(a)" },
                     { title: "Título" },
